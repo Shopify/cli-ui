0aed97d381df0816bd2ea374d685ac6f4f1a8eac
Chris Hall <chall8908@gmail.com>
2018-11-19 12:03:46 -0600

    Add flag to confirm that puts no first

--- lib/cli/ui/prompt.rb
+++ lib/cli/ui/prompt.rb
@@ -91,8 +91,10 @@ module CLI
         # Confirmation question
         #   CLI::UI::Prompt.confirm('Is the sky blue?')
         #
-        def confirm(question)
-          ask_interactive(question, %w(yes no)) == 'yes'
+        #   CLI::UI::Prompt.confirm('Do a dangerous thing?', default_yes: false)
+        #
+        def confirm(question, default_yes: true)
+          ask_interactive(question, default_yes ? %w(yes no) : %w(no yes)) == 'yes'
         end
 
         private
