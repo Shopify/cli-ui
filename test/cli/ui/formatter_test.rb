5c0b69ec015fbc027c2fb6eeb082152c4358f0b3
Chris Hall <chall8908@gmail.com>
2018-11-26 12:05:38 -0600

    Better checks for literal closing braces

--- test/cli/ui/formatter_test.rb
+++ test/cli/ui/formatter_test.rb
@@ -29,6 +29,38 @@ module CLI
         assert_equal(expected, ex.message)
       end
 
+      def test_out_of_order_braces
+        input = '}}{{blue:foo'
+        ex = assert_raises(CLI::UI::Formatter::FormatError) do
+          CLI::UI::Formatter.new(input).format
+        end
+        expected = 'Mismatched braces in input'
+        assert_equal(input, ex.input)
+        assert_equal(-1, ex.index)
+        assert_equal(expected, ex.message)
+      end
+
+      def test_leading_literal_closing_braces
+        input = '}} foo'
+        expected = "\e[0m}} foo"
+        actual = CLI::UI::Formatter.new(input).format
+        assert_equal(expected, actual)
+      end
+
+      def test_trailing_literal_closing_braces
+        input = 'foo}}'
+        expected = "\e[0mfoo}}"
+        actual = CLI::UI::Formatter.new(input).format
+        assert_equal(expected, actual)
+      end
+
+      def test_extra_closing_braces
+        input = '{{blue:foo}}}}'
+        expected = "\e[0;94mfoo\e[0m}}"
+        actual = CLI::UI::Formatter.new(input).format
+        assert_equal(expected, actual)
+      end
+
       def test_invalid_funcname
         input = "{{nope:text}}"
         ex = assert_raises(CLI::UI::Formatter::FormatError) do
