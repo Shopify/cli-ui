d321f7d0637f4dc2d9888d0145ef95e3be4cfbaa
Chris Hall <chall8908@gmail.com>
2019-09-20 16:09:09 -0500

    Cache winsize; Lazy load SIGWINCH trap

--- lib/cli/ui/terminal.rb
+++ lib/cli/ui/terminal.rb
@@ -7,52 +7,40 @@ module CLI
       DEFAULT_WIDTH = 80
       DEFAULT_HEIGHT = 24
 
-      @console = begin
-                   IO.respond_to?(:console) && IO.console
-                 rescue Errno::EIO
-                   false
-                 end
-
       # Returns the width of the terminal, if possible
       # Otherwise will return DEFAULT_WIDTH
       #
       def self.width
-        @width ||= begin
-                     if @console
-                       width = @console.winsize[1]
-                       width.zero? ? DEFAULT_WIDTH : width
-                     else
-                       DEFAULT_WIDTH
-                     end
-                   rescue Errno::EIO
-                     DEFAULT_WIDTH
-                   end
+        @winsize[1]
       end
 
       # Returns the width of the terminal, if possible
       # Otherwise, will return DEFAULT_HEIGHT
       #
       def self.height
-        @height ||= begin
-                      if @console
-                        height = @console.winsize[0]
-                        height.zero? ? DEFAULT_HEIGHT : height
-                      else
-                        DEFAULT_HEIGHT
-                      end
-                    rescue Errno::EIO
-                      DEFAULT_HEIGHT
-                    end
+        @winsize[0]
       end
 
-      def self.reset_cache
-        @width = nil
-        @height = nil
+      def self.winsize
+        @winsize ||= begin
+                       winsize = IO.console.winsize
+                       setup_winsize_trap
+
+                       if winsize.any?(&:zero?)
+                         [DEFAULT_HEIGHT, DEFAULT_WIDTH]
+                       else
+                         winsize
+                       end
+                     rescue
+                       [DEFAULT_HEIGHT, DEFAULT_WIDTH]
+                     end
+      end
+
+      def self.setup_winsize_trap
+        @_winsize_trap ||= Signal.trap('WINCH') do
+          @winsize = nil
+        end
       end
     end
   end
-end
-
-Signal.trap('WINCH') do
-  CLI::UI::Terminal.reset_cache
 end
