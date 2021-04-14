f3d742b4367be47030c4f4bafa3e625bfd8ec4c5
Lisa Ugray <lisa.ugray@shopify.com>
2021-04-14 08:38:59 -0400

    Clean up overly precise SpinGroupTest

--- test/cli/ui/spinner/spin_group_test.rb
+++ test/cli/ui/spinner/spin_group_test.rb
@@ -5,12 +5,11 @@ module CLI
     module Spinner
       class SpinGroupTest < MiniTest::Test
         def test_spin_group
-          out, err = capture_io do
+          _out, err = capture_io do
             CLI::UI::StdoutRouter.ensure_activated
 
             sg = SpinGroup.new
-            sg.add('sleeping') do
-              sleep(CLI::UI::Spinner::PERIOD * 2.5)
+            sg.add('s') do
               true
             end
 
@@ -18,22 +17,14 @@ module CLI
           end
 
           assert_equal('', err)
-          match_lines(
-            out,
-            /⠋ sleeping/,
-            /⠙/,
-            /⠹/,
-            /✓/
-          )
         end
 
         def test_spin_group_auto_debrief_false
-          out, err = capture_io do
+          _out, err = capture_io do
             CLI::UI::StdoutRouter.ensure_activated
 
             sg = SpinGroup.new(auto_debrief: false)
-            sg.add('sleeping') do
-              sleep(CLI::UI::Spinner::PERIOD * 2.5)
+            sg.add('s') do
               true
             end
 
@@ -41,27 +32,6 @@ module CLI
           end
 
           assert_equal('', err)
-          match_lines(
-            out,
-            /⠋ sleeping/,
-            /⠙/,
-            /⠹/,
-            /✓/
-          )
-        end
-
-        private
-
-        def match_lines(out, *patterns)
-          # newline, or cursor-down
-          lines = out.split(/\n|\x1b\[\d*B/)
-
-          # Assert all patterns are matched
-          assert_equal(patterns.size, lines.size)
-          patterns.each_with_index do |pattern, index|
-            line = CLI::UI::ANSI.strip_codes(lines[index])
-            assert_match(pattern, line, "pattern number #{index} doesn't match line number #{index} in the output")
-          end
         end
       end
     end
