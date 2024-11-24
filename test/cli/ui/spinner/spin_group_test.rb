a884b254d4bfbed525eb2363b73500d4c39dcad0
Tim Perkins <tim.perkins@shopify.com>
2024-11-24 13:53:35 -0500

    Add support to stop SpinGroup and show interrupt debrief

--- test/cli/ui/spinner/spin_group_test.rb
+++ test/cli/ui/spinner/spin_group_test.rb
@@ -135,7 +135,7 @@ module CLI
 
             sg.add('Interruptible task') do
               started_queue.push(true)
-              10.times { sleep(0.1) }
+              sleep(1)
               task_completed = true
             rescue Interrupt
               task_interrupted = true
@@ -143,6 +143,7 @@ module CLI
             end
 
             t = Thread.new { sg.wait }
+            t.report_on_exception = false
 
             # Wait for task to start
             started_queue.pop
@@ -153,6 +154,155 @@ module CLI
             assert_raises(Interrupt) { t.join }
             refute(task_completed, 'Task should not have completed')
             assert(task_interrupted, 'Task should have been interrupted')
+          end
+        end
+
+        def test_spin_group_stop
+          capture_io do
+            CLI::UI::StdoutRouter.ensure_activated
+            sg = SpinGroup.new
+
+            task_started = false
+            task_completed = false
+
+            sg.add('Stoppable task') do
+              task_started = true
+              sleep(1)
+              task_completed = true
+            end
+
+            t = Thread.new { sg.wait }
+
+            # Wait for task to start
+            sleep(0.1) until task_started
+
+            # Stop the spin group
+            sg.stop
+
+            t.join
+
+            refute(task_completed, 'Task should not complete after stop')
+            assert(sg.stopped?, 'SpinGroup should be marked as stopped')
+            refute(sg.all_succeeded?, 'Tasks should not be marked as succeeded after stop')
+          end
+        end
+
+        def test_spin_group_nested_stop
+          capture_io do
+            CLI::UI::StdoutRouter.ensure_activated
+            sg = SpinGroup.new
+
+            sg.add('Outer task') do
+              sg.stop
+              true
+            end
+
+            refute(sg.wait, 'SpinGroup#wait should return false when stopped')
+            assert(sg.stopped?, 'SpinGroup should be marked as stopped')
+          end
+        end
+
+        def test_spin_group_interrupt_with_debrief
+          capture_io do
+            CLI::UI::StdoutRouter.ensure_activated
+            sg = SpinGroup.new(interrupt_debrief: true)
+            task_interrupted = false
+            debrief_called = false
+
+            # Use Queue for thread-safe signaling
+            started_queue = Queue.new
+
+            sg.failure_debrief do |title, _exception, _out, _err|
+              assert_equal('Failed task', title)
+              debrief_called = true
+            end
+
+            sg.add('Failed task') do
+              TASK_FAILED
+            end
+
+            sg.add('Interruptible task') do
+              started_queue.push(true)
+              sleep(1)
+            rescue Interrupt
+              task_interrupted = true
+              raise
+            end
+
+            t = Thread.new { sg.wait }
+            t.report_on_exception = false
+
+            # Wait for task to start
+            started_queue.pop
+            sleep(0.1) # Small delay to ensure we're in sleep
+            t.raise(Interrupt)
+
+            # The interrupt should propagate since we didn't stop
+            assert_raises(Interrupt) { t.join }
+            assert(task_interrupted, 'Task should have been interrupted')
+            assert(debrief_called, 'Debrief should have been called')
+          end
+        end
+
+        def test_spin_group_interrupt_without_debrief
+          capture_io do
+            CLI::UI::StdoutRouter.ensure_activated
+            sg = SpinGroup.new(interrupt_debrief: false)
+
+            # Use Queue for thread-safe signaling
+            started_queue = Queue.new
+
+            debrief_called = false
+            sg.failure_debrief do
+              debrief_called = true
+            end
+
+            sg.add('Failed task') do
+              TASK_FAILED
+            end
+            sg.add('Interruptible task') do
+              started_queue.push(true)
+              sleep(1)
+              false
+            end
+
+            t = Thread.new { sg.wait }
+            t.report_on_exception = false
+
+            # Wait for task to actually start
+            started_queue.pop
+            sleep(0.1) # Small delay to ensure we're in sleep
+
+            # Interrupt should be raised through
+            t.raise(Interrupt)
+            assert_raises(Interrupt) { t.join }
+
+            refute(debrief_called, 'failure_debrief should not be called when interrupt_debrief is false')
+          end
+        end
+
+        def test_task_on_done_callback
+          capture_io do
+            CLI::UI::StdoutRouter.ensure_activated
+            sg = SpinGroup.new
+
+            callback_executed = false
+            task_completed = false
+
+            sg.add('Task with callback') do |task|
+              task.on_done do |completed_task|
+                callback_executed = true
+                assert_equal('Task with callback', completed_task.title)
+                assert(completed_task.done)
+                assert(completed_task.success)
+              end
+              task_completed = true
+              true
+            end
+
+            assert(sg.wait)
+            assert(task_completed, 'Task should have completed')
+            assert(callback_executed, 'on_done callback should have been executed')
           end
         end
       end
